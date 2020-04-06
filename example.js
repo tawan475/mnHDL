d4568d4861246700e266b59c1d46ec27f98e44f4
tawan475 <43491902+tawan475@users.noreply.github.com>
2020-04-06 10:01:44 +0700

    Update example.js

--- example.js
+++ example.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
-const nhentaidl = require('nhentaidl');
+const nhdl = require('nhentaidownloader');
 var args = process.argv.slice(2);
 if (!args[0] || args[0].match(/[^0-9]/g)) throw Error('First argrument must be number and valid nhentai ID.');
 
 var ID = args[0];
 
-nhentaidl(ID).then(buffer => {
+nhdl(ID).then(buffer => {
   fs.writeFileSync(`./${ID}.zip`, buffer);
 });
