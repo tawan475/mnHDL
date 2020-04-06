70934bb27f727ab7665eeb08bf5885615320cdfb
tawan475 <43491902+tawan475@users.noreply.github.com>
2020-04-06 09:36:14 +0700

    Add files via upload

--- example.js
+++ example.js
@@ -1,4 +1,10 @@
-const mnHDL = require('./mnHDL');
-const Path = `${__dirname}/${ID}.zip`;
+const fs = require('fs');
+const nhentaidl = require('nhentaidl');
+var args = process.argv.slice(2);
+if (!args[0] || args[0].match(/[^0-9]/g)) throw Error('First argrument must be number and valid nhentai ID.');
 
-mnHDL(Path, "177013")
+var ID = args[0];
+
+nhentaidl(ID).then(buffer => {
+  fs.writeFileSync(`./${ID}.zip`, buffer);
+});
